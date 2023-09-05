0f8bcd9035c261cf1b12305e5036d1a66a484019
Lahiru Amarasooriya <lahirudx@gmail.com>
2023-09-05 07:44:12 +0530

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Lahiru Amarasooriya on 2023-09-05.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
